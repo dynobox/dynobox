cb032c8e03358ad67fe8b02480e5ef786ec4de12
bkarpinos <bkarpinos@gmail.com>
2026-04-21 22:27:22 -0400

    test: add test baseline setup

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -2,18 +2,18 @@
 
 const RESET = '\x1b[0m';
 const BOLD = '\x1b[1m';
-const ORANGE = '\x1b[38;5;208m';
+const PURPLE = '\x1b[38;5;141m';
 const DIM = '\x1b[2m';
 
 console.error(`
-  ${BOLD}${ORANGE}dynobox${RESET}
+  ${BOLD}${PURPLE}dynobox${RESET}
 
   Cross-harness testing for multi-step agent flows.
 
   ${DIM}This package is a placeholder. Dynobox is under active development.${RESET}
 
-  Follow along:  ${ORANGE}https://dynobox.dev${RESET}
-  GitHub:        ${ORANGE}https://github.com/dynobox/dynobox${RESET}
+  Follow along:  ${PURPLE}https://dynobox.dev${RESET}
+  GitHub:        ${PURPLE}https://github.com/dynobox/dynobox${RESET}
 `);
 
 process.exit(1);
